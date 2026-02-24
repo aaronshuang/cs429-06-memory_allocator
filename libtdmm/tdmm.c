15b465ffc7205598c751d2368e6e5b7d87874e29
aaronshuang <aaronshuang1@gmail.com>
2026-02-23 23:04:25 -0600

    tdmm.c implemented

--- libtdmm/tdmm.c
+++ libtdmm/tdmm.c
@@ -1,14 +1,27 @@
 #include "tdmm.h"
 
+static alloc_strat_e current_strat;
+
 void t_init(alloc_strat_e strat) {
-	// TODO: Implement this
+	current_strat = strat;
+    
+    // Initializing with 1 page
+    size_t initial_size = 4096; 
+    
+    if (strat == FIRST_FIT) first_fit_init(initial_size);
+    else if (strat == BEST_FIT) best_fit_init(initial_size);
+    else if (strat == WORST_FIT) worst_fit_init(initial_size);
 }
 
 void *t_malloc(size_t size) {
-	// TODO: Implement this
-	return NULL;
+    if (current_strat == FIRST_FIT) return first_fit_malloc(size);
+    if (current_strat == BEST_FIT) return best_fit_malloc(size);
+    if (current_strat == WORST_FIT) return worst_fit_malloc(size);
+    return NULL;
 }
 
 void t_free(void *ptr) {
-  	// TODO: Implement this
+    if (current_strat == FIRST_FIT) first_fit_free(ptr);
+    else if (current_strat == BEST_FIT) best_fit_free(ptr);
+    else if (current_strat == WORST_FIT) worst_fit_free(ptr);
 }
